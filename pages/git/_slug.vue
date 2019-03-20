63624f7bd555088e4ce2f04ee656f2c9b934da4e
Lycheejam <lycheejamwow@gmail.com>
2019-03-21 02:26:30 +0900

    add axios get function to slug page

--- pages/git/_slug.vue
+++ pages/git/_slug.vue
@@ -5,12 +5,25 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   layout: 'default',
   data() {
     return {
-      model: '#Hello World'
+      model: null
     }
+  },
+  mounted: function() {
+    axios
+      .get('http://localhost:3000/testdoc/test1.md')
+      .then(function(response) {
+        this.model = response.data
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error)
+      })
   }
 }
 </script>
