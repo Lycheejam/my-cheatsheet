ef2f5eeedd0ff181bf575ff7c00fdcc1d720b4e7
Lycheejam <lycheejamwow@gmail.com>
2019-03-21 15:07:41 +0900

    やっていき

--- pages/git/_slug.vue
+++ pages/git/_slug.vue
@@ -1,29 +1,17 @@
-<template>
-  <div>
-    <div v-html="$md.render(model)" />
-  </div>
+<template lang="md">
+  <div v-html="$md.render(model)" />
 </template>
 
 <script>
 import axios from 'axios'
+import model from './assets/testdoc/test1.md'
 
 export default {
   layout: 'default',
   data() {
     return {
-      model: null
+      model: model
     }
-  },
-  mounted: function() {
-    axios
-      .get('http://localhost:3000/testdoc/test1.md')
-      .then(function(response) {
-        this.model = response.data
-      })
-      .catch(function(error) {
-        // handle error
-        console.log(error)
-      })
   }
 }
 </script>
