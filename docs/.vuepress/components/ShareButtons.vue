329a0fa8c57ed606ab56a8e35f4e696e5f26fa87
Lycheejam <lycheejamwow@gmail.com>
2019-04-04 20:55:35 +0900

    Create ShareButtons.vue

---
+++ docs/.vuepress/components/ShareButtons.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="ShareButtons">
+    <ul>
+      <li>
+        Twitter
+      </li>
+      <li>
+        Facebook
+      </li>
+      <li>
+        B!
+      </li>
+      <li>
+        LINE
+      </li>
+      <li>
+        Pocket
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShareButtons'
+}
+</script>
+
+<style>
+#ShareButtons li{
+  list-style: none;
+  float: left;
+}
+</style>
